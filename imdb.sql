3f46f017cdd51c66704793970066bde24a626c13
Bredoto <serg.rumyantsev@gmail.com>
2021-02-22 20:27:46 +0200

    Table was created

--- imdb.sql
+++ imdb.sql
@@ -1,33 +1,40 @@
 drop database imdb;
 create database  IF NOT EXISTS imdb  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 use imdb;
+
 CREATE TABLE Actor (
     ActorID int NOT NULL,
-    name varchar(255) NOT NULL,  
-    FilmID int,
-    PRIMARY KEY (ActorID),
-    FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+    name varchar(255) NOT NULL, 
+    PRIMARY KEY (ActorID)
 );
 
 CREATE TABLE Director (
     DirectorID int NOT NULL,
-    name varchar(255) NOT NULL, 
-    FilmID int,
-    PRIMARY KEY (DirectorID),
-    FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (DirectorID)
 );
 
 CREATE TABLE Film (
     FilmID int NOT NULL,
     title  varchar(255) NOT NULL,
+    PRIMARY KEY (FilmID)    
+);
+
+CREATE TABLE Movie (
+    MovieID int NOT NULL,
+    title  varchar(255) NOT NULL,
     ActorID int,
-    DirectorID int,
-    PRIMARY KEY (FilmID),
+    FilmID int,
+    PRIMARY KEY (MovieID),
     FOREIGN KEY (ActorID) REFERENCES Actor(ActorID),
-    FOREIGN KEY (DirectorID) REFERENCES Director(DirectorID)
+    FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
 );
 
 
+
+-- FOREIGN KEY (ActorID) REFERENCES Actor(ActorID),
+   --  FOREIGN KEY (DirectorID) REFERENCES Director(DirectorID)FOREIGN KEY (ActorID) REFERENCES Actor(ActorID),
+      -- FOREIGN KEY (DirectorID) REFERENCES Director(DirectorID)
 
 
  /*insert into Film (title,ActorID,DirectorID) values ('Inception',1,1),('Titanic',1,2);
